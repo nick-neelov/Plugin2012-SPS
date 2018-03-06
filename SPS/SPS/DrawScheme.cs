fa4b93f01672c9348cbb50a27e031a66ee3673e1
nick-neelov <nickneelov@gmail.com>
2018-03-06 14:51:07 +0300

    Добавлен файл класса для отрисовки схем

---
+++ SPS/SPS/DrawScheme.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neelov.AutocadPlugin
+{
+	class DrawScheme
+	{
+
+		// hello
+	}
+}
